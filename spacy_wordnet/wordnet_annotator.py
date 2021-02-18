e6b6e754f4dc22602b9cab6e572933b41cf9bb9a
Andrea Sterbini <a.sterbini@gmail.com>
2021-02-18 20:31:47 +0100

    now it works with both spacy 2 and 3

--- spacy_wordnet/wordnet_annotator.py
+++ spacy_wordnet/wordnet_annotator.py
@@ -1,18 +1,18 @@
-from spacy.tokens.doc import Doc
-from spacy.tokens.token import Token
-from spacy.language import Language
+
+from spacy.tokens.doc       import Doc
+from spacy.tokens.token     import Token
+from spacy.parts_of_speech  import *
+from spacy.language         import Language
 
 from spacy_wordnet.wordnet_domains import Wordnet, load_wordnet_domains
 
-
-@Language.factory("wordnet")
-class WordnetAnnotator(object):
+class WordnetAnnotator:
     __FIELD = 'wordnet'
 
     def __init__(self, nlp, name):
+        self.__lang = nlp.lang
         Token.set_extension(WordnetAnnotator.__FIELD, default=None, force=True)
         load_wordnet_domains()
-        self.__lang = nlp.lang
 
     def __call__(self, doc: Doc):
         for token in doc:
@@ -20,3 +20,11 @@ class WordnetAnnotator(object):
             token._.set(WordnetAnnotator.__FIELD, wordnet)
 
         return doc
+
+#TODO: handle also spacy2
+try:
+    @Language.factory("wordnet")
+    def make_WA(nlp, name):
+        return WordnetAnnotator(nlp, name)
+except:
+    pass
