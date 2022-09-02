1858f2600abe6ab01d913775c9cb27781d2542ca
Ian Thompson <ian.thompson@hrblock.com>
2022-09-02 12:42:39 -0500

    modified:   spacy_wordnet/wordnet_domains.py 	- fixed type error in __find_lemmas() method by swapping self.__synsets attribute with self.synsets(...) method 	- pep8 character limit fix in __find_lemmas() method

--- spacy_wordnet/wordnet_domains.py
+++ spacy_wordnet/wordnet_domains.py
@@ -110,4 +110,8 @@ class Wordnet(object):
         return [domain for synset in self.synsets() for domain in get_domains_for_synset(synset)]
 
     def __find_lemmas(self):
-        return [lemma for synset in self.__synsets for lemma in synset.lemmas(lang=self.__lang)]
+        return [
+            lemma for synset in self.synsets() for lemma in synset.lemmas(
+                lang=self.__lang
+            )
+        ]
