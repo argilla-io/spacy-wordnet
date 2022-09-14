40fbb044c5cbb418a486f89b5fcf2d0bfdd55aee
Ian Thompson <ian.thompson@hrblock.com>
2022-09-14 16:06:58 -0500

    modified:   tests/test_wordnet_annotator.py 	- added assert to test that list of pos args will return expected results

--- tests/test_wordnet_annotator.py
+++ tests/test_wordnet_annotator.py
@@ -35,6 +35,9 @@ class WordnetAnnotatorTest(unittest.TestCase):
         assert token._.wordnet.lemmas()
         assert token._.wordnet.wordnet_domains()
 
+        actual_none_synsets = set(token._.wordnet.synsets(pos=None))
+        expected_none_synsets = {}
+
         actual_verb_synsets = set(token._.wordnet.synsets(pos="verb"))
         expected_verb_synsets = {wn.synset('abridge.v.01'),
                                  wn.synset('compress.v.02'),
@@ -56,6 +59,23 @@ class WordnetAnnotatorTest(unittest.TestCase):
         actual_adj_synsets = set(token._.wordnet.synsets(pos="adj"))
         expected_adj_synsets = set()
         assert actual_adj_synsets == expected_adj_synsets
+
+        actual_verb_noun_synsets = set(token._.wordnet.synsets(
+            pos=["verb", "noun"])
+        )
+        expected_verb_noun_synsets = {wn.synset('abridge.v.01'),
+                                      wn.synset('compress.v.02'),
+                                      wn.synset('condense.v.07'),
+                                      wn.synset('contract.v.01'),
+                                      wn.synset('contract.v.04'),
+                                      wn.synset('contract.v.06'),
+                                      wn.synset('narrow.v.01'),
+                                      wn.synset('shrink.v.04'),
+                                      wn.synset('sign.v.04'),
+                                      wn.synset('contract.n.01'),
+                                      wn.synset('contract.n.02'),
+                                      wn.synset('contract.n.03')}
+        assert actual_verb_noun_synsets == expected_verb_noun_synsets
 
     def test_generate_variants_from_domain_list(self):
 
